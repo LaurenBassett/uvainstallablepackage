f987cf5f628506ea1e59f39d7f51cf127ed80cb3
laurenbassett <laurenebassett@gmail.com>
2023-02-05 18:23:38 -0500

    changed setup.py and test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 sys.path.append('.')
 import shared as sh
 
@@ -6,3 +7,31 @@ def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+
+class TestFunction(unittest.TestCase):
+    def test_whitespace(self):
+        # Compress Whitespace
+        output = sh.space_compress("word       word      word")
+        self.assertEqual(output, "word word word")
+
+    def test_newline(self):
+        # If passed multiline string, it should return a single line
+        output = sh.space_compress("word \n word \n word")
+        self.assertEqual(output, "word word word")
+    def test_exception_int(self):
+        #If passed an int, it should run an assert Error
+        try:
+            output = sh.space_compress(1)
+        except AssertionError as e:
+            self.assertEqual(type(e), AssertionError)
+            
+    def test_exception_bool(self):
+        #If passed a boolean, it should run an assert error
+        try:
+            output = sh.space_compress(True)
+        except AssertionError as e:
+            self.assertEqual(type(e), AssertionError)
+
+
+if __name__ == '__main__':
+    unittest.main()
